b7a52f97deab43a4f02f57a437cd40ad47d51f3e
dirueg <nhs741@gmail.com>
2023-11-30 23:48:58 +0900

    simple date format apply

--- MyApplication/app/src/main/java/com/example/myapplication/SignatureEntity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SignatureEntity.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Entity
 data class SignatureEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val userName: String,
     val signature: ByteArray, // Bitmap을 ByteArray로 저장
-    val timestamp: Long = System.currentTimeMillis()
+    val currentDate: String = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
 )
