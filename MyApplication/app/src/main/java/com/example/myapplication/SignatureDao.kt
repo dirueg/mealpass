4215a6f9e13a4d2699dfa5c09803ae1ddf32a541
dirueg <nhs741@gmail.com>
2023-12-02 21:21:34 +0900

    기능 추가는 22

---
+++ MyApplication/app/src/main/java/com/example/myapplication/SignatureDao.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): LiveData<List<User>>
+
+    @Insert
+    fun insert(user: User)
+
+    @Delete
+    fun delete(user: User)
+}
+
+@Dao
+public interface SignatureDao {
+    @Insert
+    fun insertSignature(signature: SignatureEntity)
+
+    @Query("SELECT * FROM signature_database")
+    fun getAllSignatures(): LiveData<List<SignatureEntity>>
+
+    @Query("SELECT * FROM signature_database WHERE currentDate BETWEEN :startDate AND :endDate")
+    fun getSignaturesInRange(startDate: String, endDate: String): LiveData<List<SignatureEntity>>
+    @Query("SELECT * FROM signature_database WHERE currentDate BETWEEN :startDate AND :endDate ORDER BY currentDate")
+    fun getSignaturesSortedByDate(startDate: String, endDate: String): LiveData<List<SignatureEntity>>
+
+    @Query("SELECT * FROM signature_database WHERE currentDate BETWEEN :startDate AND :endDate ORDER BY userName")
+    fun getSignaturesSortedByName(startDate: String, endDate: String): LiveData<List<SignatureEntity>>
+
+
+}
